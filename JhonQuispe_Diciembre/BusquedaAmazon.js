737dbc0ac6ffc0a5834f0201253801433e2c580e
jkeny <jkeny_1985@hotmail.com>
2020-12-13 18:41:31 -0500

    Commit del ejercio 01 - Clase 1 Grupo Intermedio

---
+++ JhonQuispe_Diciembre/BusquedaAmazon.js
@@ -0,0 +1,9 @@
+descripcion: "Buscar Zapatillas en Amazon"
+PreCondicion: "La web de amazon disponible"
+Pasos:
+    Ir a amazon.com
+    Buscar zapatillas
+    Seleccionar el primer resultado
+    Verificar que este bien
+PostCondicion: "Sistema muestra resultados"
+Resultado esperado: "que el primer resultado muestre detalle de la zapatilla"
